afc90e2a1f9070daa55ef3607f10d4267cbdea02
Urvashi933 <srivastavaurvashi933@gmail.com>
2026-04-03 18:40:27 +0530

    Refactor UserRepository to use JpaRepository
    Refactored UserRepository to extend JpaRepository instead of CrudRepository. Removed custom findById method and added findByName method.

--- src/main/java/com/jpmc/midascore/repository/UserRepository.java
+++ src/main/java/com/jpmc/midascore/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.jpmc.midascore.repository;
 
 import com.jpmc.midascore.entity.UserRecord;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;  // FIX: CrudRepository → JpaRepository
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends CrudRepository<UserRecord, Long> {
-    UserRecord findById(long id);
+@Repository  // Optional with JpaRepository
+public interface UserRepository extends JpaRepository<UserRecord, Long> {  // Better base interface
+  
+    // findById(Long id) inherited from JpaRepository - REMOVE custom method
+    UserRecord findByName(String name);  // Custom query method
 }
